66898413274f3f177f5a77dc3bf7b6c30450998c
newbietot <nasa.sly14@gmail.com>
2021-01-28 19:25:36 +0700

    update

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,378 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "com.napolitano.cordova.plugin.intent.IntentPlugin",
+      "file": "plugins/com.napolitano.cordova.plugin.intent/www/android/IntentPlugin.js",
+      "pluginId": "com.napolitano.cordova.plugin.intent",
+      "clobbers": [
+        "IntentPlugin"
+      ]
+    },
+    {
+      "id": "cordova-open-native-settings.Settings",
+      "file": "plugins/cordova-open-native-settings/www/settings.js",
+      "pluginId": "cordova-open-native-settings",
+      "clobbers": [
+        "cordova.plugins.settings"
+      ]
+    },
+    {
+      "id": "cordova-plugin-android-permissions.Permissions",
+      "file": "plugins/cordova-plugin-android-permissions/www/permissions.js",
+      "pluginId": "cordova-plugin-android-permissions",
+      "clobbers": [
+        "cordova.plugins.permissions"
+      ]
+    },
+    {
+      "id": "cordova-plugin-app-launcher.Launcher",
+      "file": "plugins/cordova-plugin-app-launcher/www/Launcher.js",
+      "pluginId": "cordova-plugin-app-launcher",
+      "clobbers": [
+        "plugins.launcher"
+      ]
+    },
+    {
+      "id": "cordova-plugin-badge.Badge",
+      "file": "plugins/cordova-plugin-badge/www/badge.js",
+      "pluginId": "cordova-plugin-badge",
+      "clobbers": [
+        "cordova.plugins.notification.badge"
+      ]
+    },
+    {
+      "id": "cordova-plugin-buildinfo.BuildInfo",
+      "file": "plugins/cordova-plugin-buildinfo/www/buildinfo.js",
+      "pluginId": "cordova-plugin-buildinfo",
+      "clobbers": [
+        "BuildInfo"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.Camera",
+      "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "Camera"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.CameraPopoverOptions",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverOptions"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.camera",
+      "file": "plugins/cordova-plugin-camera/www/Camera.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "navigator.camera"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.CameraPopoverHandle",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverHandle"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.DirectoryEntry",
+      "file": "plugins/cordova-plugin-file/www/DirectoryEntry.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.DirectoryEntry"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.DirectoryReader",
+      "file": "plugins/cordova-plugin-file/www/DirectoryReader.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.DirectoryReader"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.Entry",
+      "file": "plugins/cordova-plugin-file/www/Entry.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.Entry"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.File",
+      "file": "plugins/cordova-plugin-file/www/File.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.File"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileEntry",
+      "file": "plugins/cordova-plugin-file/www/FileEntry.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileEntry"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileError",
+      "file": "plugins/cordova-plugin-file/www/FileError.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileError"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileReader",
+      "file": "plugins/cordova-plugin-file/www/FileReader.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileReader"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileSystem",
+      "file": "plugins/cordova-plugin-file/www/FileSystem.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileSystem"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileUploadOptions",
+      "file": "plugins/cordova-plugin-file/www/FileUploadOptions.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileUploadOptions"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileUploadResult",
+      "file": "plugins/cordova-plugin-file/www/FileUploadResult.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileUploadResult"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.FileWriter",
+      "file": "plugins/cordova-plugin-file/www/FileWriter.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.FileWriter"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.Flags",
+      "file": "plugins/cordova-plugin-file/www/Flags.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.Flags"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.LocalFileSystem",
+      "file": "plugins/cordova-plugin-file/www/LocalFileSystem.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.LocalFileSystem"
+      ],
+      "merges": [
+        "window"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.Metadata",
+      "file": "plugins/cordova-plugin-file/www/Metadata.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.Metadata"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.ProgressEvent",
+      "file": "plugins/cordova-plugin-file/www/ProgressEvent.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.ProgressEvent"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.fileSystems",
+      "file": "plugins/cordova-plugin-file/www/fileSystems.js",
+      "pluginId": "cordova-plugin-file"
+    },
+    {
+      "id": "cordova-plugin-file.requestFileSystem",
+      "file": "plugins/cordova-plugin-file/www/requestFileSystem.js",
+      "pluginId": "cordova-plugin-file",
+      "clobbers": [
+        "window.requestFileSystem"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.resolveLocalFileSystemURI",
+      "file": "plugins/cordova-plugin-file/www/resolveLocalFileSystemURI.js",
+      "pluginId": "cordova-plugin-file",
+      "merges": [
+        "window"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.isChrome",
+      "file": "plugins/cordova-plugin-file/www/browser/isChrome.js",
+      "pluginId": "cordova-plugin-file",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-file.androidFileSystem",
+      "file": "plugins/cordova-plugin-file/www/android/FileSystem.js",
+      "pluginId": "cordova-plugin-file",
+      "merges": [
+        "FileSystem"
+      ]
+    },
+    {
+      "id": "cordova-plugin-file.fileSystems-roots",
+      "file": "plugins/cordova-plugin-file/www/fileSystems-roots.js",
+      "pluginId": "cordova-plugin-file",
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-file.fileSystemPaths",
+      "file": "plugins/cordova-plugin-file/www/fileSystemPaths.js",
+      "pluginId": "cordova-plugin-file",
+      "merges": [
+        "cordova"
+      ],
+      "runs": true
+    },
+    {
+      "id": "cordova-plugin-local-notification.LocalNotification",
+      "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
+      "pluginId": "cordova-plugin-local-notification",
+      "clobbers": [
+        "cordova.plugins.notification.local"
+      ]
+    },
+    {
+      "id": "cordova-plugin-local-notification.LocalNotification.Core",
+      "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
+      "pluginId": "cordova-plugin-local-notification",
+      "clobbers": [
+        "cordova.plugins.notification.local.core",
+        "plugin.notification.local.core"
+      ]
+    },
+    {
+      "id": "cordova-plugin-local-notification.LocalNotification.Util",
+      "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
+      "pluginId": "cordova-plugin-local-notification",
+      "merges": [
+        "cordova.plugins.notification.local.core",
+        "plugin.notification.local.core"
+      ]
+    },
+    {
+      "id": "cordova-plugin-media.MediaError",
+      "file": "plugins/cordova-plugin-media/www/MediaError.js",
+      "pluginId": "cordova-plugin-media",
+      "clobbers": [
+        "window.MediaError"
+      ]
+    },
+    {
+      "id": "cordova-plugin-media.Media",
+      "file": "plugins/cordova-plugin-media/www/Media.js",
+      "pluginId": "cordova-plugin-media",
+      "clobbers": [
+        "window.Media"
+      ]
+    },
+    {
+      "id": "cordova-plugin-navigationbar-color.navigationbar",
+      "file": "plugins/cordova-plugin-navigationbar-color/www/navigationbar.js",
+      "pluginId": "cordova-plugin-navigationbar-color",
+      "clobbers": [
+        "window.NavigationBar"
+      ]
+    },
+    {
+      "id": "cordova-plugin-sdcard.sdcard",
+      "file": "plugins/cordova-plugin-sdcard/www/plugin.js",
+      "pluginId": "cordova-plugin-sdcard",
+      "clobbers": [
+        "cordova.plugins.sdcard"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    },
+    {
+      "id": "cordova-plugin-tts.tts",
+      "file": "plugins/cordova-plugin-tts/www/tts.js",
+      "pluginId": "cordova-plugin-tts",
+      "clobbers": [
+        "TTS"
+      ]
+    },
+    {
+      "id": "cordova-plugin-websocket.websocket",
+      "file": "plugins/cordova-plugin-websocket/www/websocket.js",
+      "pluginId": "cordova-plugin-websocket",
+      "clobbers": [
+        "WebSocket"
+      ]
+    },
+    {
+      "id": "cordova-plugin-x-toast.Toast",
+      "file": "plugins/cordova-plugin-x-toast/www/Toast.js",
+      "pluginId": "cordova-plugin-x-toast",
+      "clobbers": [
+        "window.plugins.toast"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "com.napolitano.cordova.plugin.intent": "0.1.2",
+    "cordova-open-native-settings": "1.5.2",
+    "cordova-plugin-android-permissions": "1.1.2",
+    "cordova-plugin-app-launcher": "0.4.0",
+    "cordova-plugin-badge": "0.8.8",
+    "cordova-plugin-buildinfo": "4.0.0",
+    "cordova-plugin-camera": "5.0.1",
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-file": "6.0.2",
+    "cordova-plugin-local-notification": "0.9.0-beta.2",
+    "cordova-plugin-media": "5.0.3",
+    "cordova-plugin-navigationbar-color": "0.0.8",
+    "cordova-plugin-sdcard": "0.0.1",
+    "cordova-plugin-statusbar": "2.4.3",
+    "cordova-plugin-tts": "0.2.3",
+    "cordova-plugin-webpack": "1.0.5",
+    "cordova-plugin-websocket": "0.12.2",
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-x-toast": "2.7.2",
+    "cordova-plugin-proguard": "2.2.0",
+    "cordova-plugin-crypt-file-android": "1.4.2"
+  };
+});
---
+++ plugins/cordova-plugin-webpack/CHANGELOG.md
@@ -0,0 +1,146 @@
+## [1.0.5](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v1.0.4...v1.0.5) (2020-05-17)
+
+### Bug Fixes
+
+- **deps:** bump is-ua-webview from 1.0.3 to 1.0.4 ([353af4c](https://github.com/kotarella1110/cordova-plugin-webpack/commit/353af4cd47dfd96180e3b3299db9dd70ffb4d6ae))
+- **deps:** bump is-ua-webview from 1.0.3 to 1.0.5 ([8cf8648](https://github.com/kotarella1110/cordova-plugin-webpack/commit/8cf8648d1b7d9007fc4209849d7131bdb03a02d2))
+
+## [1.0.4](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v1.0.3...v1.0.4) (2020-05-10)
+
+### Bug Fixes
+
+- use absolute path to cordova script ([#137](https://github.com/kotarella1110/cordova-plugin-webpack/issues/137)) ([e457d0e](https://github.com/kotarella1110/cordova-plugin-webpack/commit/e457d0ed731b4ee0b62b93e1c047ae2223a604ef))
+
+## [1.0.3](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v1.0.2...v1.0.3) (2020-05-09)
+
+### Bug Fixes
+
+- **deps:** bump @sindresorhus/is from 2.1.0 to 2.1.1 ([d6fdbaf](https://github.com/kotarella1110/cordova-plugin-webpack/commit/d6fdbaf085a0952c51cc2b0656bf1ad23ff54324))
+- **deps:** bump current-device from 0.10.0 to 0.10.1 ([09652d0](https://github.com/kotarella1110/cordova-plugin-webpack/commit/09652d07ad1b2560454787f0eef6615473044918))
+- **deps:** bump is-ua-webview from 1.0.1 to 1.0.2 ([ea2f037](https://github.com/kotarella1110/cordova-plugin-webpack/commit/ea2f037acf2b59f4c3b2ad812d9200792beb7d2c))
+- **deps:** bump is-ua-webview from 1.0.2 to 1.0.3 ([8b497ff](https://github.com/kotarella1110/cordova-plugin-webpack/commit/8b497ff7e7671e1ff0d5df7579cd962685ea2eb5))
+- **deps:** bump source-map-support from 0.5.16 to 0.5.17 ([87148f0](https://github.com/kotarella1110/cordova-plugin-webpack/commit/87148f0d2763df34b0511877f9d1f052a64899cf))
+- **deps:** bump source-map-support from 0.5.17 to 0.5.18 ([f90da63](https://github.com/kotarella1110/cordova-plugin-webpack/commit/f90da63003e65765882edadf5c1e03e4d6742267))
+- **deps:** bump source-map-support from 0.5.18 to 0.5.19 ([46afed7](https://github.com/kotarella1110/cordova-plugin-webpack/commit/46afed734f1a271b32a7ad7c4106284b0911176a))
+
+<a name="1.0.2"></a>
+
+## [1.0.2](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v1.0.1...v1.0.2) (2020-04-01)
+
+### Bug Fixes
+
+- fixed bug of cordova serve command ([#52](https://github.com/kotarella1110/cordova-plugin-webpack/issues/52)) ([2afe61a](https://github.com/kotarella1110/cordova-plugin-webpack/commit/2afe61a))
+
+<a name="1.0.1"></a>
+
+## [1.0.1](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v1.0.0...v1.0.1) (2020-03-24)
+
+<a name="1.0.0"></a>
+
+# [1.0.0](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v1.0.0-alpha.2...v1.0.0) (2020-03-24)
+
+### Features
+
+- **type:** added type defininition for cordova hooks context ([419bc08](https://github.com/kotarella1110/cordova-plugin-webpack/commit/419bc08)), closes [#11](https://github.com/kotarella1110/cordova-plugin-webpack/issues/11)
+
+<a name="1.0.0-alpha.2"></a>
+
+# [1.0.0-alpha.2](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v1.0.0-alpha.1...v1.0.0-alpha.2) (2019-10-03)
+
+### Features
+
+- added hook for peerDeps installed check ([d30db39](https://github.com/kotarella1110/cordova-plugin-webpack/commit/d30db39))
+
+<a name="1.0.0-alpha.1"></a>
+
+# [1.0.0-alpha.1](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v1.0.0-alpha.0...v1.0.0-alpha.1) (2019-10-02)
+
+### Bug Fixes
+
+- fixed webpack options validation error in multiple config ([2f891c5](https://github.com/kotarella1110/cordova-plugin-webpack/commit/2f891c5)), closes [#21](https://github.com/kotarella1110/cordova-plugin-webpack/issues/21)
+
+### Features
+
+- **serve:** added processing to exit signals ([4234f93](https://github.com/kotarella1110/cordova-plugin-webpack/commit/4234f93))
+
+<a name="1.0.0-alpha.0"></a>
+
+# [1.0.0-alpha.0](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.4.7...v1.0.0-alpha.0) (2019-09-30)
+
+### Features
+
+- added support various configuration types ([50685ff](https://github.com/kotarella1110/cordova-plugin-webpack/commit/50685ff)), closes [#21](https://github.com/kotarella1110/cordova-plugin-webpack/issues/21) [#23](https://github.com/kotarella1110/cordova-plugin-webpack/issues/23)
+
+<a name="0.4.7"></a>
+
+## [0.4.7](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.4.6...v0.4.7) (2019-09-19)
+
+### Bug Fixes
+
+- rewrited callback func in promise from callback func ([fd493bf](https://github.com/kotarella1110/cordova-plugin-webpack/commit/fd493bf))
+
+<a name="0.4.6"></a>
+
+## [0.4.6](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.4.5...v0.4.6) (2019-08-22)
+
+### Bug Fixes
+
+- resolved a small bug ([c8c592a](https://github.com/kotarella1110/cordova-plugin-webpack/commit/c8c592a)), closes [#20](https://github.com/kotarella1110/cordova-plugin-webpack/issues/20)
+
+<a name="0.4.5"></a>
+
+## [0.4.5](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.4.1...v0.4.5) (2019-08-22)
+
+### Bug Fixes
+
+- **server:** fixed browser platform not to be required ([ef616ac](https://github.com/kotarella1110/cordova-plugin-webpack/commit/ef616ac)), closes [#16](https://github.com/kotarella1110/cordova-plugin-webpack/issues/16)
+
+<a name="0.4.1"></a>
+
+## [0.4.1](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.3.1...v0.4.1) (2019-06-12)
+
+### Bug Fixes
+
+- **server:** fixed to detect mobile browser and webview ([c61e087](https://github.com/kotarella1110/cordova-plugin-webpack/commit/c61e087)), closes [#15](https://github.com/kotarella1110/cordova-plugin-webpack/issues/15)
+
+### Features
+
+- **server:** added support browser for livereload (hmr) ([e7f54e1](https://github.com/kotarella1110/cordova-plugin-webpack/commit/e7f54e1))
+- **server:** added support browser for livereload (hmr) ([7d2c081](https://github.com/kotarella1110/cordova-plugin-webpack/commit/7d2c081))
+
+<a name="0.3.1"></a>
+
+## [0.3.1](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.3.0...v0.3.1) (2019-05-18)
+
+<a name="0.3.0"></a>
+
+# [0.3.0](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.2.0...v0.3.0) (2019-05-16)
+
+<a name="0.2.0"></a>
+
+# [0.2.0](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.1.2...v0.2.0) (2019-05-14)
+
+<a name="0.1.2"></a>
+
+## [0.1.2](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.1.1...v0.1.2) (2019-05-14)
+
+<a name="0.1.1"></a>
+
+## [0.1.1](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.1.0...v0.1.1) (2019-05-14)
+
+<a name="0.1.0"></a>
+
+# [0.1.0](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.0.2...v0.1.0) (2019-05-13)
+
+### Features
+
+- **server:** added webpack serve ([02de7a4](https://github.com/kotarella1110/cordova-plugin-webpack/commit/02de7a4))
+- **utils:** added cordova config parser ([92c9063](https://github.com/kotarella1110/cordova-plugin-webpack/commit/92c9063))
+
+<a name="0.0.2"></a>
+
+## [0.0.2](https://github.com/kotarella1110/cordova-plugin-webpack/compare/v0.0.1...v0.0.2) (2018-05-08)
+
+<a name="0.0.1"></a>
+
+## 0.0.1 (2018-05-08)
